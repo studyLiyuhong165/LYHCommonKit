c497a74a7256538cef7d54a8c92510ce8f72738f
studyLiyuhong165 <study_liyuhong165@163.com>
2022-05-02 20:39:02 +0800

    初始化远程仓库

---
+++ Classes/YHConst.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+NS_ASSUME_NONNULL_BEGIN
+@interface YHConst : NSObject
+extern NSString * const YHUserTokenKey;
+extern NSString * const AppleLoginId;
+@end
+NS_ASSUME_NONNULL_END
---
+++ Classes/YHConst.m
@@ -0,0 +1,5 @@
+#import "YHConst.h"
+@implementation YHConst
+NSString * const YHUserTokenKey = @"com.lyh.tokenkey";
+NSString * const AppleLoginId = @"AppleLoginId";
+@end
